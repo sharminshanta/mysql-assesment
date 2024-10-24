0ed06f02ac7a9da381b4e12b187347c18ae8ef88
Sharmin Shanta <mimetyshanta@gmail.com>
2024-10-24 22:48:25 +0600

    COmbine two tables.

--- combine_two_table.sql
+++ combine_two_table.sql
@@ -1,1 +1,10 @@
 
+--- Write a solution to report the first name, last name, city, and state of each person in the Person table.
+--- If the address of a personId is not present in the Address table, report null instead.
+
+SELECT 
+    FirstName, LastName, City, State
+FROM
+    Person p
+        LEFT JOIN
+    Address a ON p.PersonId = a.PersonId;
