c689ee10c3892810e68489e14d6f9bcfce16f09c
Sharmin Shanta <mimetyshanta@gmail.com>
2024-10-25 16:10:37 +0600

    Basic Select Query.

--- basic_select.sql
+++ basic_select.sql
@@ -1,1 +1,25 @@
+--- Write a solution to find the ids of products that are both low fat and recyclable.
+Database Example:
+Input: 
+Products table:
++-------------+----------+------------+
+| product_id  | low_fats | recyclable |
++-------------+----------+------------+
+| 0           | Y        | N          |
+| 1           | Y        | Y          |
+| 2           | N        | Y          |
+| 3           | Y        | Y          |
+| 4           | N        | N          |
++-------------+----------+------------+
+Output: 
++-------------+
+| product_id  |
++-------------+
+| 1           |
+| 3           |
++-------------+
 
+Solution:
+SELECT name 
+FROM customer 
+WHERE referee_id != 2 OR referee_id IS NULL;
